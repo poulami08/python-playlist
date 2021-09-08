4af5c7206279c81b41baa681f4a0864814ccafea
Poulami Mandal <poulami.kv@gmail.com>
2021-09-09 01:10:53 +0530

    first commit

---
+++ chapter-1/03_playsong.py
@@ -0,0 +1,4 @@
+# Install an external module and use it to perform an operation of your interest.
+
+from playsound import playsound
+playsound('G:/Users/MRIDUL/Desktop/POULAMI/python_course/chapter-1/yes.mp3')
