d42245ab61b12e8e23a1f8b172fe62688791e02b
Poulami Mandal <poulami.kv@gmail.com>
2021-09-24 08:50:26 +0530

    updated 0.2

--- chapter-5/09_change.py
+++ chapter-5/09_change.py
@@ -5,4 +5,4 @@ S = {8, 7, 12, "Harry", [1, 2]}
 S[4]=5
 print(S)
 
-# ans- first of all the question was wrong in the first place we cannot put list inside a set ad if insted of list a tuple was present then also we cannot change the values of tuple as we cannot modify the tuple.
+# ans- first of all the question was wrong in the first place we cannot put list inside a set and if insted of list a tuple was present then also we cannot change the values of tuple as we cannot modify the tuple.
